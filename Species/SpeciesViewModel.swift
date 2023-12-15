48d353f42458095d33d5268d7a6fae3e26907a1f
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-15 10:30:39 -0300

    Creating SpeciesViewModel

--- Species/SpeciesViewModel.swift
+++ Species/SpeciesViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+@MainActor
+class SpeciesViewModel: ObservableObject {
+    struct Returned: Codable {
+        var next: String? //  It's an optional value because it will be null on last page.
+        var results: [Species]
+    }
+    
+    @Published var speciesArray: [Species] = []
+    var urlString = "https://swapi.dev/api/species"
+    
+    
+    
+}
