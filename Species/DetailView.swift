32af5ba7bd9bb427dfeac4c030bd9c20a4ba61f9
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-15 11:10:50 -0300

    Creating a DetailView

---
+++ Species/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Species
+//
+//  Created by Christian Manzaraz on 15/12/2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
