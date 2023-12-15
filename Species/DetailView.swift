fab5dd08892b09456866220acb0ca42aa3c8e553
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-15 11:11:33 -0300

    Creting a DetailView

--- Species/DetailView.swift
+++ Species/DetailView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct DetailView: View {
+    let species: Species
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading) {
+            Text(species.name)
+                .font(.largeTitle)
+                .bold()
+            
+            Rectangle()
+                .frame(height: 2)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.gray)
+            
+            Group {
+                HStack (alignment: .top) {
+                    Text("Classification: ")
+                        .bold()
+                    Text(species.classification)
+                }
+                HStack (alignment: .top) {
+                    Text("Designation: ")
+                        .bold()
+                    Text(species.designation)
+                }
+                HStack (alignment: .top) {
+                    Text("Height: ")
+                        .bold()
+                    Text(species.average_height)
+                    Spacer()
+                    Text("Lifespan: ")
+                        .bold()
+                    Text(species.average_height)
+                }
+                HStack (alignment: .top) {
+                    Text("Language: ")
+                        .bold()
+                    Text(species.language)
+                }
+                HStack (alignment: .top) {
+                    Text("Skin Colors:")
+                        .bold()
+                    Text(species.skin_colors)
+                }
+                HStack (alignment: .top) {
+                    Text("Hair colors:")
+                        .bold()
+                    Text(species.hair_colors)
+                }
+                HStack (alignment: .top) {
+                    Text("Eye Colors:")
+                        .bold()
+                    Text(species.eye_colors)
+                }
+            }
+            .font(.title3)
+            Spacer()
+        }
+        .padding()
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(species: Species(name: "Swifter", classification: "Coder", designation: "sentient", average_height: "175", average_lifespan: "83", language: "Swift", skin_colors: "various", hair_colors: "various or none", eye_colors: "blue, green, brown, black, hazel, gray or violet"))
 }
