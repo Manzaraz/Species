82cdcecc1e74b12a42ec8209d50c781dc470867c
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-15 09:54:57 -0300

    Initial Commit

---
+++ Species/SpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeciesApp.swift
+//  Species
+//
+//  Created by Christian Manzaraz on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
